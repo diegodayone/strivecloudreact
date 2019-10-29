5567877871c94dd3f7ffec9b26e67d685083f743
Diego Banovaz <diego@strive.school>
2019-10-29 10:47:01 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is our live code for our first Web App
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://strive.school"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Learn more about strive.school
         </a>
       </header>
     </div>
